90c40ddfa090db332a2c24bc5f31a0d73c77c19b
KalebLawrence <ksl00007@mix.wvu.edu>
2024-03-21 15:45:33 -0400

    Password Validation I don't think we need login.js anymore.

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -4,14 +4,92 @@ import LoginStyle from './CSS Modules/LoginPage.module.css';
 
 const LoginPage = () => {
   const [showLoginForm, setShowLoginForm] = useState(true);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [usernameError, setUsernameError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
 
+    // I assume the submission to database will go here as well but not sure.
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Add your form submission logic here
+  
+    let usernameErrorMsg = '';
+    let passwordErrorMsg = '';
+    let emailErrorMsg = '';
+    let confirmPasswordErrorMsg = '';
+  
+    // Validation for username
+    if (username.length < 10) {
+      usernameErrorMsg = 'Username must be at least 10 characters long.';
+    }
+  
+    // Validation for email probably needs work
+    if (!email.includes('@') || !email.endsWith('.com')) {
+      emailErrorMsg = 'Please enter a valid email address.';
+    }
+  
+    // Validation for password
+    if (!/[A-Z]/.test(password) || !/\d/.test(password)) {
+      passwordErrorMsg = 'Must contain at least one capital letter and one number.';
+    }
+  
+    // Validation for confirm password
+    if (confirmPassword !== password) {
+      confirmPasswordErrorMsg = 'Passwords do not match.';
+    }
+  
+    setUsernameError(usernameErrorMsg);
+    setPasswordError(passwordErrorMsg);
+    setEmailError(emailErrorMsg);
+    setConfirmPasswordError(confirmPasswordErrorMsg);
+  
+    // If any field has an error, prevent form submission
+    if (usernameErrorMsg || passwordErrorMsg || emailErrorMsg || confirmPasswordErrorMsg) {
+      return;
+    }
+
+    // Set error messages
+    setUsernameError(usernameErrorMsg);
+    setPasswordError(passwordErrorMsg);
+    setEmailError(emailErrorMsg);
+    setConfirmPasswordError(confirmPasswordErrorMsg);
+
+    if (usernameErrorMsg || passwordErrorMsg || emailErrorMsg || confirmPasswordErrorMsg) {
+      return;
+    }
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
   };
 
   const toggleForm = () => {
     setShowLoginForm(!showLoginForm);
+    setUsername('');
+    setPassword('');
+    setEmail('');
+    setConfirmPassword('');
+
+    setUsernameError('');
+    setPasswordError('');
+    setEmailError('');
+    setConfirmPasswordError('');
   };
 
   return (
@@ -23,14 +101,26 @@ const LoginPage = () => {
         {showLoginForm && (
           <form className={LoginStyle.form} id="login" onSubmit={handleSubmit}>
             <h1 className={LoginStyle.form_title}>Login</h1>
-            <div className={`${LoginStyle.form_message} ${LoginStyle.form_input_error_message}`}></div>
-            <div className={LoginStyle.form_input_group}>
-              <input type="text" className={LoginStyle.form_input} autoFocus placeholder="Username or email" />
-              <div className={LoginStyle.form_input_error_message}></div>
+            <div className={`${LoginStyle.form_input_group}`}>
+              <input 
+                type="text" 
+                className={LoginStyle.form_input} 
+                autoFocus 
+                placeholder="Username or email" 
+                value={username} 
+                onChange={handleUsernameChange} 
+              />
+              <div className={LoginStyle.form_input_error_message}>{usernameError}</div>
             </div>
             <div className={LoginStyle.form_input_group}>
-              <input type="password" className={LoginStyle.form_input} placeholder="Password" />
-              <div className={LoginStyle.form_input_error_message}></div>
+              <input 
+                type="password" 
+                className={LoginStyle.form_input} 
+                placeholder="Password" 
+                value={password} 
+                onChange={handlePasswordChange} 
+              />
+              <div className={LoginStyle.form_input_error_message}>{passwordError}</div>
             </div>
             <button className={LoginStyle.form_button} type="submit">Continue</button>
             <p className={LoginStyle.form_text}>
@@ -39,122 +129,65 @@ const LoginPage = () => {
           </form>
         )}
 
-        {!showLoginForm && (
-          <form className={LoginStyle.form} id="createAccount" onSubmit={handleSubmit}>
-            <h1 className={LoginStyle.form_title}>Create Account</h1>
-            <div className={`${LoginStyle.form_message} ${LoginStyle.form_message_error}`}></div>
-            <div className={LoginStyle.form_input_group}>
-              <input type="text" className={LoginStyle.form_input} autoFocus placeholder="Username" />
-              <div className={LoginStyle.form_input_error_message}></div>
-            </div>
-            <div className={LoginStyle.form_input_group}>
-              <input type="text" className={LoginStyle.form_input} autoFocus placeholder="Email Address" />
-              <div className={LoginStyle.form_input_error_message}></div>
-            </div>
-            <div className={LoginStyle.form_input_group}>
-              <input type="password" className={LoginStyle.form_input} autoFocus placeholder="Password" />
-              <div className={LoginStyle.form_input_error_message}></div>
-            </div>
-            <div className={LoginStyle.form_input_group}>
-              <input type="password" className={LoginStyle.form_input} autoFocus placeholder="Confirm Password" />
-              <div className={LoginStyle.form_input_error_message}></div>
-            </div>
-            <button className={LoginStyle.form_button} type="submit">Continue</button>
-            <p className={LoginStyle.form_text}>
-              <button className={LoginStyle.form_link} onClick={toggleForm}>Already have an account? Sign in</button>
-            </p>
-          </form>
-        )}
+{!showLoginForm && (
+  <form className={LoginStyle.form} id="createAccount" onSubmit={handleSubmit}>
+    <h1 className={LoginStyle.form_title}>Create Account</h1>
+    <div className={`${LoginStyle.form_message} ${LoginStyle.form_message_error}`}></div>
+    <div className={LoginStyle.form_input_group}>
+      <input 
+        type="text" 
+        className={LoginStyle.form_input} 
+        autoFocus 
+        placeholder="Username" 
+        value={username} 
+        onChange={handleUsernameChange} 
+      />
+      <div className={LoginStyle.form_input_error_message}>{usernameError}</div>
+    </div>
+    <div className={LoginStyle.form_input_group}>
+      <input 
+        type="text" 
+        className={LoginStyle.form_input} 
+        autoFocus 
+        placeholder="Email Address" 
+        value={email} 
+        onChange={handleEmailChange} 
+      />
+      <div className={LoginStyle.form_input_error_message}>{emailError}</div>
+    </div>
+    <div className={LoginStyle.form_input_group}>
+      <input 
+        type="password" 
+        className={LoginStyle.form_input} 
+        placeholder="Password" 
+        value={password} 
+        onChange={handlePasswordChange} 
+      />
+      <div className={LoginStyle.form_input_error_message}>{passwordError}</div>
+    </div>
+    <div className={LoginStyle.form_input_group}>
+      <input 
+        type="password" 
+        className={LoginStyle.form_input} 
+        placeholder="Confirm Password" 
+        value={confirmPassword} 
+        onChange={handleConfirmPasswordChange} 
+      />
+      <div className={LoginStyle.form_input_error_message}>{confirmPasswordError}</div>
+    </div>
+    <button className={LoginStyle.form_button} type="submit">Continue</button>
+    <p className={LoginStyle.form_text}>
+      <button className={LoginStyle.form_link} onClick={toggleForm}>Already have an account? Sign in</button>
+    </p>
+  </form>
+)}
       </div>
-      {/* Adding the waves */}
+      {}
       <div className={LoginStyle.wave}></div>
       <div className={LoginStyle.wave}></div>
       <div className={LoginStyle.wave}></div>
     </div>
   );
 };
-
-/*
-const LoginStyle = {
-  container: {
-    width: '400px',
-    maxWidth: '400px',
-    margin: '1rem auto', // Center the form horizontally
-    padding: '2rem',
-    boxShadow: '0 0 40px rgba(0, 0, 0, 0.2)',
-    borderRadius: '4px',
-    background: '#ffffff',
-    fontFamily: 'Rubik, sans-serif',
-  },
-  form: {
-    textAlign: 'center',
-  },
-  form__title: {
-    marginBottom: '2rem',
-    textAlign: 'center',
-  },
-  form__message: {
-    textAlign: 'center',
-    marginBottom: '1rem',
-  },
-  'form__message--error': {
-    color: '#cc3333',
-  },
-  form__input_group: {
-    marginBottom: '1rem',
-  },
-  form__input: {
-    display: 'block',
-    width: '100%',
-    padding: '0.75rem',
-    boxSizing: 'border-box',
-    borderRadius: '4px',
-    border: '1px solid #3B4C5A',
-    outline: 'none',
-    background: '#eeeeee',
-    transition: 'background 0.2s, border-color 0.2s',
-  },
-  form__input_error_message: {
-    marginTop: '0.5rem',
-    fontSize: '0.85rem',
-    color: '#cc3333',
-  },
-  form__button: {
-    width: '100%',
-    padding: '1rem 2rem',
-    fontSize: '1.1rem',
-    color: '#ffffff',
-    border: 'none',
-    borderRadius: '4px',
-    outline: 'none',
-    cursor: 'pointer',
-    background: '#3B4C5A',
-  },
-  form__button_hover: {
-    background: '#007f67',
-  },
-  form__button_active: {
-    transform: 'scale(0.98)',
-  },
-  form__text: {
-    textAlign: 'center',
-  },
-  form__link: {
-    color: '#252c6a',
-    textDecoration: 'none',
-    cursor: 'pointer',
-    border: 'none',
-    background: 'none',
-    outline: 'none',
-  },
-  'form__link:hover': {
-    textDecoration: 'underline',
-  },
-  'form--hidden': {
-    display: 'none',
-  },
-};
-
-*/
 
 export default LoginPage;
